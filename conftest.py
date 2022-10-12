b16216263509bf8bf6d3d8154829ec93385a01ec
serg.pudikov <pudikov_serg77@mail.ru>
2022-10-12 12:11:39 -0400

    structure improvements

--- conftest.py
+++ conftest.py
@@ -41,12 +41,44 @@ def driver_loan_setup(driver_init):
     login.click()
     time.sleep(5)
 
+    switch_to_frame(0)
+    switch_to_frame(0)
+    wait_xpath('//*[@id="ImportLoanButton"]').click()
+    switch_to_parent_frame()
+    switch_to_parent_frame()
+    time.sleep(2)
+
+    switch_to_frame('BusinessChannelSelection')
+    wait_xpath('//*[@id="BusinessChannelDropDownList"]').click()
+    wait_xpath('//*[@id="BusinessChannelDropDownList"]/option[3]').click()
+    switch_to_default_content()
+
+    wait_xpath('//*[@id="btnOkay"]').click()
+    time.sleep(2)
+
+    driver.switch_to.frame(1)
+    driver.switch_to.frame(0)
+    time.sleep(5)
+    upload_file = driver.find_element(By.ID, "AjaxFileUpload_Html5InputFile")
+    upload_file.send_keys('C:/Users/serg.pudikov/PycharmProjects/test_integra_partner/docs/kensp.xml')
+    wait_xpath('//*[@id="AjaxFileUpload_UploadOrCancelButton"]').click()
+    switch_to_default_content()
+
+    global loannumber
+    loannumber = wait_xpath('//*[@id="Row1"]/td[2]').text
+
     yield driver
     driver.close()
 
 
 @pytest.fixture
+def loannumber_import():
+    return loannumber
+
+
+@pytest.fixture
 def driver_tests(driver_init):
+
     driver.get('http://t-partner.admortgage.us/Default.aspx')
 
     wait_frame_id('contentFrame')
@@ -57,12 +89,14 @@ def driver_tests(driver_init):
     username.send_keys('serg.pudikov@admortgage.com')
     password.send_keys('Welcome1@')
     login.click()
+    time.sleep(5)
 
     switch_to_frame(0)
     switch_to_frame(0)
-    wait_xpath('//*[@id="SearchTextBox"]').send_keys('9006745')  # Change to loannumber
+    wait_xpath('//*[@id="SearchTextBox"]').send_keys('9006772')  # Change to loannumber
     wait_xpath('//*[@id="SearchButton"]').click()
     switch_to_default_content()
+    time.sleep(2)
 
     switch_to_frame(0)
     switch_to_frame(0)
@@ -73,6 +107,7 @@ def driver_tests(driver_init):
     action.double_click(a)
     action.perform()
     switch_to_default_content()
+    time.sleep(2)
 
     yield driver
     driver.close()
