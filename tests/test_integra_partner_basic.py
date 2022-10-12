a486113f886b9741e7d223bc1995b35f04e20fe5
serg.pudikov <pudikov_serg77@mail.ru>
2022-10-12 12:28:38 -0400

    structure improvements

--- tests/test_integra_partner_basic.py
+++ tests/test_integra_partner_basic.py
@@ -12,13 +12,11 @@ from conftest import wait_frame_id
 from conftest import switch_to_frame
 from conftest import switch_to_parent_frame
 from conftest import switch_to_default_content
-from conftest import loannumber_import
 
 
 @allure.feature('Loan Setup')
 @allure.story('Loan setup')
 def test_loan_setup(driver_loan_setup, loannumber_import):
-
     with allure.step('Loan number'):
         allure.attach(loannumber_import)
 
@@ -29,7 +27,7 @@ def test_loan_setup(driver_loan_setup, loannumber_import):
 
 @allure.feature('Basic Tests')
 @allure.story('Import Loan')
-def test_import_loan(driver_tests, loannumber_import):
+def test_import_loan(driver_tests):  # loannumber_import
     try:
         loanid = wait_xpath('//*[@id="Row1"]/td[2]')
         assert loanid.text == '9006772'  # change to loannumber
