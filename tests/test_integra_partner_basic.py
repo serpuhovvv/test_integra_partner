499a6f47d49e4d342bee137913df6fa278cac4a5
serg.pudikov <pudikov_serg77@mail.ru>
2022-12-05 14:01:43 -0500

    bug fixes and structure improvements

--- tests/test_integra_partner_basic.py
+++ tests/test_integra_partner_basic.py
@@ -17,28 +17,6 @@ from conftest import select_option
 
 
 @allure.feature('Basic Tests')
-@allure.story('Import Loan')
-def test_import_loan(driver_loan_setup, loannumber_import):
-
-    try:
-        loanid = wait_xpath('//*[@id="Row1"]/td[2]')
-        with allure.step('Loan Number'):
-            allure.attach(loannumber_import, name='loannumber', attachment_type=AttachmentType.TEXT)
-        print(loannumber_import)
-        assert loanid.text == loannumber_import
-
-    except Exception as ex:
-        with allure.step('Error screenshot'):
-            allure.attach(driver_loan_setup.get_screenshot_as_png(), name='error_screenshot',
-                          attachment_type=AttachmentType.PNG)
-        print(ex)
-        assert False
-
-    finally:
-        exit_loan()
-
-
-@allure.feature('Basic Tests')
 @allure.story('Create Loan')
 def test_create_loan(driver_init):
 
@@ -66,6 +44,28 @@ def test_create_loan(driver_init):
 
     finally:
         driver_init.quit()
+
+
+@allure.feature('Basic Tests')
+@allure.story('Import Loan')
+def test_import_loan(driver_loan_setup, loannumber_import):
+
+    try:
+        loanid = wait_xpath('//*[@id="Row1"]/td[2]')
+        with allure.step('Loan Number'):
+            allure.attach(loannumber_import, name='loannumber', attachment_type=AttachmentType.TEXT)
+        print(loannumber_import)
+        assert loanid.text == loannumber_import
+
+    except Exception as ex:
+        with allure.step('Error screenshot'):
+            allure.attach(driver_loan_setup.get_screenshot_as_png(), name='error_screenshot',
+                          attachment_type=AttachmentType.PNG)
+        print(ex)
+        assert False
+
+    finally:
+        exit_loan()
 
 
 @allure.feature('Basic Tests')
